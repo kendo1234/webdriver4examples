475fc9b42f556c7c067b66eaa6b658e9f3f30726
Ken Talbot <ken.talbot@moneysupermarket.com>
2023-01-04 09:03:09 +0000

    initial commit, driver config

---
+++ README.md
@@ -0,0 +1,34 @@
+# Webdriver 4 Demo
+
+## Description
+
+Just a quick tutorial-level set of tests to brush up on latest concepts of Selenium Webdriver
+
+## Getting Started
+
+### Dependencies
+
+* Java 11
+* Junit 5
+* Webdriver Manager
+* Selenium 4
+
+### Installing
+
+* Basic Maven project
+```
+mvn install
+```
+
+### Executing program
+
+* Run from within IDE
+* Run in CLI
+```
+mvn test
+```
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [Pluralsight course](https://app.pluralsight.com/library/courses/automated-web-testing-selenium-webdriver-java)
---
+++ src/main/java/org/example/WebDriverDemo.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverDemo {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void firstTest() {
+
+        driver.get("http://pluralsight.com");
+    }
+}
